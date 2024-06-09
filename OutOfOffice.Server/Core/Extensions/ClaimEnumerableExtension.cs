24a6bb2c22e8a2b249b24ada2b3abc41457404d3
Sasha Semachkin <az195753@gmail.com>
2024-06-09 17:10:28 +0300

    Расширение чтобы удобно получать клеймы с userId и Role

---
+++ OutOfOffice.Server/Core/Extensions/ClaimEnumerableExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace OutOfOffice.Server.Core.Extensions;
+
+public static class ClaimEnumerableExtension
+{
+    public static ulong GetUserId(this IEnumerable<Claim> claims) => 
+        Convert.ToUInt64(claims.First(p=>p.Type == "userId").Value);
+
+    public static string GetUserRole(this IEnumerable<Claim> claims) =>
+        claims.First(p => p.Type == ClaimTypes.Role).Value;
+}

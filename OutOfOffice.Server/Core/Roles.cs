06e23d31abfea09a2c9eb679bb74db6250af6b99
Sasha Semachkin <az195753@gmail.com>
2024-06-18 23:17:14 +0300

    Добавил класс с статическими названиями ролей для возможного удобного использования в будущем

---
+++ OutOfOffice.Server/Core/Roles.cs
@@ -0,0 +1,11 @@
+﻿using OutOfOffice.Core.Models.Enums;
+
+namespace OutOfOffice.Server.Core;
+
+public static class Roles
+{
+    public const string EmployeeRole = nameof(EmployeePosition.Employee);
+    public const string HrManagerRole = nameof(EmployeePosition.HrManager);
+    public const string ProjectManagerRole = nameof(EmployeePosition.ProjectManager);
+    public const string AdministratorRole = nameof(EmployeePosition.Administrator);
+}
